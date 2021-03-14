7f2ac9218d893235a729406b784ccee47c0f55bd
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-03-13 20:36:26 -0500

    added js files for routes and server

---
+++ server.js
@@ -0,0 +1,22 @@
+// dependencies
+const express = require('express');
+const path = require('path');
+
+// tells node that we are creating express server
+const app = express();
+
+// sets initial port
+const PORT = process.env.PORT || 3000;
+
+// sets up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// router
+require('./routes/html')(app);
+require('./routes/api')(app);
+
+// listener
+app.listen(PORT, () => {
+    console.log(`listening on PORT: ${PORT}`);
+});
