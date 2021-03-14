fbd561616476d391a80b67494852f3444a8d1528
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-03-13 21:29:55 -0500

    working on server

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ const PORT = process.env.PORT || 3000;
 // sets up express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // router
 require('./routes/html')(app);
 require('./routes/api')(app);
+require('./public')(app);
 
 // listener
 app.listen(PORT, () => {
